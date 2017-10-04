a7d58900b41bdd93048cca4ac04f6ffbe19382b5
xandyn <mail.vagapov@gmail.com>
2017-10-04 16:40:01 +0300

    init commit

---
+++ ios/Pods/Target Support Files/Pods-RNapp-tvOS/Pods-RNapp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNapp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_RNapp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-RNapp/Pods-RNapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RNapp : NSObject
+@end
+@implementation PodsDummy_Pods_RNapp
+@end
